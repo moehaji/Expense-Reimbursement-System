886aebfab32a60477123ed95763eaec7a485ab55
bayodes <bayode.o.olaoye@gmail.com>
2022-05-07 14:37:40 -0400

    Working it out

--- app/src/main/java/com/revature/dao/IReimbursementDao.java
+++ app/src/main/java/com/revature/dao/IReimbursementDao.java
@@ -2,15 +2,26 @@ package com.revature.dao;
 
 import com.revature.models.Reimbursement;
 
+import java.util.List;
+
 public interface IReimbursementDao {
 
     //Create
     public void createReimbursement(Reimbursement r);
 
     //Read
+    public List<Reimbursement> viewAllReimbursement();
+
+    public List<Reimbursement> viewSpecificReimbursement(int id);
+
+    public List<Reimbursement> viewPendingReimbursement();
+
+    public List<Reimbursement> viewAllResolvedReimbursement();
 
     //Update
+    public Reimbursement updateReimbursementStatus(Reimbursement r);
 
     //Delete
+    public void dleteReimbursement(int id);
 
 }
--- app/src/main/java/com/revature/dao/ReimbursementDaoJDBC.java
+++ app/src/main/java/com/revature/dao/ReimbursementDaoJDBC.java
@@ -5,11 +5,16 @@ package com.revature.dao;
 import com.revature.models.Reimbursement;
 import com.revature.utils.ConnectionSingleton;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReimbursementDaoJDBC implements IReimbursementDao {
 
     private ConnectionSingleton cs = ConnectionSingleton.getConnectionSingleton();
+
+    public ReimbursementDaoJDBC(Reimbursement r) {
+    }
 
     @Override
     public void createReimbursement(Reimbursement r) {
@@ -17,8 +22,73 @@ public class ReimbursementDaoJDBC implements IReimbursementDao {
 
         String sql = "insert into reimbursement " +
                 "(amount, submitted_date, resolved_date, description, reimbursement_author, reimbursement_resolver, reimbursement_status, reimbursement_type) " +
-                "values ";
+                "values (?,?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setDouble(1, r.getAmount());
+            ps.setString(2, r.getSubmittedDate());
+            ps.setString(3, r.getResolvedDate());
+            ps.setString(4, r.getDescription());
+            ps.setInt(5, r.getReimbursementAuthor());
+            ps.setInt(6, r.getReimbursementResolver());
+            ps.setInt(7, r.getReimbursementStatus());
+            ps.setInt(8, r.getReimbursementType());
+            ps.execute();
+        } catch (SQLException e ) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Reimbursement> viewAllReimbursement() {
+        Connection c = cs.getConnection();
+
+        String sql = "select * from reimbursement";
+        List<Reimbursement> reimbursementList = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = c.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                Reimbursement r = new Reimbursement(rs.getInt(1), rs.getDouble(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7),
+                        rs.getInt(8), rs.getInt(9));
+                reimbursementList.add(r);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return reimbursementList;
+    }
+
+    @Override
+    public List<Reimbursement> viewSpecificReimbursement(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Reimbursement> viewPendingReimbursement() {
+        return null;
+    }
+
+    @Override
+    public List<Reimbursement> viewAllResolvedReimbursement() {
+        return null;
+    }
+
+    @Override
+    public Reimbursement updateReimbursementStatus(Reimbursement r) {
+        return null;
+    }
+
+    @Override
+    public void dleteReimbursement(int id) {
 
     }
+
 
 }
