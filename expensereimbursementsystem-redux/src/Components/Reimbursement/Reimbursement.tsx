51b1efb22ed68dad3026f79da10c6203ec4cad98
bayodes <bayode.o.olaoye@gmail.com>
2022-05-16 12:48:16 -0400

    revert back to 5c9987d commit

--- expensereimbursementsystem-redux/src/Components/Reimbursement/Reimbursement.tsx
+++ expensereimbursementsystem-redux/src/Components/Reimbursement/Reimbursement.tsx
@@ -1,19 +1,33 @@
+<<<<<<< HEAD
 import React from "react";
 import { IReimbursement } from "../../Interfaces/IReimbursement";
 
 export const Reimbursement:React.FC<IReimbursement> = (reimbursement:IReimbursement) => {
     return(
-        <div className="reimbursement">
-            <div className="reimbursement-profile">
-                <img src="" alt="" className="reimbursement-image"/>
-                <h3 className="reimbursement-user">
-                    {reimbursement.reimbursementAuthor}
-                </h3>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <h3></h3>
             </div>
 
-            <div className="">
-                <p>{reimbursement.reimbursementType}</p>
-            </div>
         </div>
     );
+=======
+import React, {useState, useEffect} from "react";
+
+import './Reimbursement.css'
+
+import { IReimbursement } from "../../Interfaces/IReimbursement";
+
+export const Reimbursement:React.FC<IReimbursement> = (reimbursement:IReimbursement) => {
+
+    return(
+        <div className="reimbursement">
+            <h2>{reimbursement.submittedDate}</h2>
+            <p>{reimbursement.description}</p>
+            <p>{reimbursement.amount}</p>
+        </div>
+    )
+
+>>>>>>> mohamed-frontend
 }
