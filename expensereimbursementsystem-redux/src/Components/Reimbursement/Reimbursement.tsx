5c9987dbdae87a55fe557bba86ccde65ed4197bf
bayodes <bayode.o.olaoye@gmail.com>
2022-05-16 08:48:47 -0400

    Added Store file and Slices folder

--- expensereimbursementsystem-redux/src/Components/Reimbursement/Reimbursement.tsx
+++ expensereimbursementsystem-redux/src/Components/Reimbursement/Reimbursement.tsx
@@ -3,12 +3,17 @@ import { IReimbursement } from "../../Interfaces/IReimbursement";
 
 export const Reimbursement:React.FC<IReimbursement> = (reimbursement:IReimbursement) => {
     return(
-        <div>
-            <div>
-                <img src="" alt="" />
-                <h3></h3>
+        <div className="reimbursement">
+            <div className="reimbursement-profile">
+                <img src="" alt="" className="reimbursement-image"/>
+                <h3 className="reimbursement-user">
+                    {reimbursement.reimbursementAuthor}
+                </h3>
             </div>
 
+            <div className="">
+                <p>{reimbursement.reimbursementType}</p>
+            </div>
         </div>
     );
 }
