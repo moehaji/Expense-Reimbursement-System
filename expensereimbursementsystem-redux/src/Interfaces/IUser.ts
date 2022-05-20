a294db3345becdd8cf34476012a31ad718a783d4
bayodes <103948928+bayodes@users.noreply.github.com>
2022-05-20 06:15:48 -0400

    Delete expensereimbursementsystem-redux directory

--- expensereimbursementsystem-redux/src/Interfaces/IUser.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-    userID: number,
-    userName: string,
-    password?: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    role?: number,
-}
